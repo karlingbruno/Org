9ccf4e265b21677f3d6d255498b8f24dc57f0d8a
karlingbruno <30853741+karlingbruno@users.noreply.github.com>
2019-12-09 18:55:20 -0300

    Add files via upload

--- simuladorCache.py
+++ simuladorCache.py
@@ -1,37 +1,140 @@
 import numpy as np
-memoriaPrincipal = np.random.randint(0,255,128)  #gera uma lista com numeros aleatórios entre 0 e 255
+import os
+
+tamBloco = 4
+linConjunto = 2
+tamCache = 8
+tamMP = 128
+tamCelula = 8
+hits = 0
+misses = 0
+clear = lambda: os.system('clear')
+memoriaPrincipal = np.random.randint(0,(2**tamCelula) - 1,tamMP)  #gera uma lista com numeros aleatórios entre 0 e 255
 for item in memoriaPrincipal:  #transforma os numeros em binários de 8 bits
     item = format(item,'08b')
 
+
 memoriaCache = []
 i = 0
-while i <= 7:
-    memoriaCache.append(format(0,'032b'))
+while i < tamCache:
+    memoriaCache.append("002222222222222222222222222222222222222")
     i+= 1
+print("len",len(memoriaCache[0]))
+def polSubstituicao(linha):
+    if(linha % 2 == 0):
+        vish = memoriaCache[linha][2:]
+        aux2 = memoriaCache[linha][0]
+        memoriaCache[linha] = aux2 + str(1) + vish
+        vish = memoriaCache[linha+1][2:]
+        aux2 = memoriaCache[linha+1][0]
+        memoriaCache[linha+1] = aux2 + str(0) + vish
+    else:
+        vish = memoriaCache[linha][2:]
+        aux2 = memoriaCache[linha][0]
+        memoriaCache[linha] = aux2 + str(1) + vish
+        vish = memoriaCache[linha-1][2:]
+        aux2 = memoriaCache[linha-1][0]
+        memoriaCache[linha - 1] = aux2 + str(0) + vish
+        
 
-tamBloco = 4
+def leitura (endereco):
+    print(endereco)
+    conjunto = int(endereco[3:5],2)
+    print("conjunto",conjunto)
+    i = conjunto 
+    aux = 0
+    while i <= conjunto + 1 : # passa pelo conjunto da cache
+        if str(memoriaCache[i][2:7]) == str(endereco[:5]): # se o endereço está na memória cache
+            global hits
+            hits  = hits + 1
+            aux += 1
+            print("Bloco está na cache!")
+            inteiro = int(endereco[5:],2) + 1 # pegando o deslocamento
+            polSubstituicao(i)
+            print("Valor :",memoriaCache[i][7 * (inteiro) -1 :7 * (inteiro) + 7])
+        i = i + 1
 
-linConjunto = 2
+    if(aux == 0): # se for 0 não encontrou o bloco na cache
+        global misses 
+        misses = misses + 1
+        bloco = int(endereco[:5]) # descobrindo o numero do bloco
+        i = conjunto * linConjunto
+        while i <= conjunto * linConjunto + 1: #passa pelo conjunto da cache
+            if memoriaCache[i][1] == "0": # se o valor for 0 quer dizer que é o mais antigo na cache
+                substituicao = i
+            i = i + 1
+        i = (bloco * tamBloco) 
+
+        meudeus = "1"+"1"+str(format(bloco,'05b'))
+        while i < (bloco * tamBloco) + tamBloco :
+            meudeus = meudeus + str(format(memoriaPrincipal[i],'08b'))
+
+            i += 1
+        memoriaCache[substituicao] = meudeus
+        polSubstituicao(substituicao)
+        
+        
+def Escrita(endereco):
+
+    leitura(endereco)
+    escrever = input("Digite o valor a ser escrito na memória:")
+    memoriaPrincipal[int(endereco,2)] = escrever
+    conjunto = int(endereco[3:5],2)
+    i = conjunto
+    while i <= conjunto * 2 + 1:
+        if (str(memoriaCache[i][2:7]) == str(endereco[:5])):
+            linha = i
+        i = i + 1
+    print("linha",linha)
+    inteiro = int(endereco[5:],2) + 1
+    print("COnjunto :",conjunto)
+    copia = memoriaCache[linha][:7 * (inteiro)]
+    copia2 = memoriaCache[linha][7 * (inteiro) + 7:]
+    clear()
+    memoriaCache[linha] = str(copia) + str(escrever) + str(copia2)
+    print("Valor escrito na memória\n\n")
+
+
+        
+
+
+            
+
+
+
+ 
 '''
 for item in memoriaPrincipal:    #printa em hexadecimal
    print(format(item,'02X))
 '''
-'''
+
 cond = 0
-while (cond != 4):
-
-    print('1 : Ler um endereço da memória\n2 : Escrever na memória\n3 : Apresentar as estatísticas\n4 : Sair do programa') 
-    input(cond)
-
-    if (cond == 1):
-
-    elif (cond == 2):
-
-    elif (cond == 3):
-
-    elif (cond == 4)
-
+while (cond != 5):
+    print('1 : Ler um endereço da memória\n2 : Escrever na memória\n3 : Apresentar as estatísticas\n4 : Printar memórias \n5 : Sair do programa') 
+    cond = input()
+    print(cond)
+    if cond == "1":
+        poxa = str(input("Digite o endereço a ser lido\n"))
+        leitura(poxa)
+        continue
+    elif cond == "2":
+        poxa = str(input("Digite o endereço a ser escrito\n"))
+        Escrita(poxa)
+        continue
+    elif cond == "3":
+        stats()
+        continue
+    elif cond == "4":
+        clear()
+        print("Memória Principal:\n")
+        for item in memoriaPrincipal:
+            print(format(item,'08b'))
+        print("\n\nMemória Cache:\n")
+        for item in memoriaCache:
+            print(item)
+        continue
+    elif cond == "5":
+        break
     else:
         print("Opção inválida\n")
-
-'''
+        continue
